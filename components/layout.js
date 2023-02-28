780502d0abf2f636133ed229bc7a93faf1f7bbc9
geleia <geleia@mail.com>
2023-02-28 02:44:49 -0300

    joi

--- components/layout.js
+++ components/layout.js
@@ -6,65 +6,72 @@ import styles from './layout.module.css';
 
 
 const name = 'Bem vindo ao blog! :)';
-export const siteTitle = 'Blog do Geleia';  
+export const siteTitle = 'Blog do Geleia';
 
 export default function Layout({ children, home }) {
-    return (
-      <div className={styles.container}>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="blog do geleia"
-            content="personal website using Next.js"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-              siteTitle,
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <header className={styles.header}>
-          {home ? (
-            <>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="blog do geleia"
+          content="personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={450}
+              width={450}
+              alt=""
+            />
+            <h2 className={utilStyles.heading2Xl}>{name}</h2>
+          </>
+        ) : (
+          <>
+            <Link href="/">
               <Image
                 priority
-                src="/images/profile.png"
+                src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
-                height={85}
-                width={85}
+                height={450}
+                width={450}
                 alt=""
               />
-              <h2 className={utilStyles.heading2Xl}>{name}</h2>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <Image
-                  priority
-                  src="/images/profile.png"
-                  className={utilStyles.borderCircle}
-                  height={90}
-                  width={90}
-                  alt=""
-                />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
               </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/" className={utilStyles.colorInherit}>
-                  {name}
-                </Link>
-              </h2>
-            </>
-          )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">← Back to home</Link>
-          </div>
+            </h2>
+          </>
         )}
-      </div>
-    );
-  }
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+      <footer>
+          <Image
+            src="/images/github-mark-white.svg"
+            className={utilStyles.borderCircle}
+            height={20}
+            width={20} />&nbsp;<p><a href='https://github.com/geleiaa/Blog_nextjs' target="_blank">repo do projeto</a></p>
+      </footer>
+    </div>
+  );
+}
---
+++ posts/node-js-recursos.md
@@ -0,0 +1,50 @@
+---
+title: 'recursos nodejs'
+date: '2023-02-27'
+description: 'Recursos, links, artigos que eu usei e uso nos estudos javascript para backend.'
+---
+
+
+#### *Essa lista de conteúdos vai ser dividida nos tópicos:* 
+1 - [artigos](#artigos)
+
+2 - [cursos(gratuitos e pagos)](#cursosgratuitos-e-pagos)
+
+3 - [repositórios do github](#github-repos)
+
+4 - [outros](#outros)
+
+> Atualizarei sempre que achar algo novo ou relevante :)
+
+>---
+## Artigos:
+Esse artigo tem um overview de alguns frameworks mais populares para js backend: 
+> #### *https://medium.com/codex/nodejs-developer-roadmap-2022-80d33457e2f2*
+
+Vários tutoriais introdutórios sobre desenvolvimento server-side:
+> #### *https://developer.mozilla.org/pt-BR/docs/Learn/Server-side*
+
+>---
+## Cursos(gratuitos e pagos):
+Path básico sobre nodejs:
+> #### *https://www.theodinproject.com/paths/full-stack-javascript/courses/nodejs*
+
+Cursos gratuitos e pagos do Erick Wendel. Vale a pena dar uma olhada nos gratuitos:
+> #### *https://cursos.erickwendel.com.br/?origin=timeline*
+
+Esse aqui você pode ignorar o curso e ver só o roadmap que é focado em desenvolvimento backend js:
+> #### *https://codedamn.com/learning-paths/backend*
+
+>---
+## Github repos:
+Com certeza não pode faltar um pouco de conteúdo sobre js vanilla que é uma das partes mais importantes:
+> #### *https://github.com/getify/You-Dont-Know-JS/tree/1st-ed*
+> #### *https://www.youtube.com/watch?v=3PHXvlpOkf4*
+...
+- https://github.com/nodejs/nodejs.dev/tree/aa4239e87a5adc992fdb709c20aebb5f6da77f86/content/learngithb
+- https://github.com/goldbergyoni/nodebestpractices
+- https://github.com/sindresorhus/awesome-nodejs
+>---
+## Outros:
+Introdução ao node, desde a instalação até os recursos mais importantes:
+> #### *https://nodejs.dev/en/learn/introduction-to-nodejs/*
